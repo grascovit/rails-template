3746f976f4b38aaea58911af13a251e12eaabef9
Gabriel Rascovit <rascovit@gmail.com>
2024-05-19 15:53:47 -0300

    add spec for magic link token method

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,18 @@ RSpec.describe User, type: :model do
     it { is_expected.to validate_uniqueness_of(:email).case_insensitive }
     it { is_expected.to validate_uniqueness_of(:phone_number).case_insensitive }
   end
+
+  describe '#magic_link_token' do
+    before do
+      allow(ENV).to receive(:fetch).with('JWT_SECRET', nil).and_return('secret')
+    end
+
+    it 'returns a JWT token' do
+      user = create(:user)
+      token = user.magic_link_token
+      decoded_token = JWT.decode(token, 'secret', true, algorithm: 'HS256').first
+
+      expect(decoded_token['user_id']).to eq(user.id)
+    end
+  end
 end
